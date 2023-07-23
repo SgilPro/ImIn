54b15b0522fe06384ac2449340a21bbad8c1e826
Jean Chung <jean619215@gmail.com>
2023-07-23 14:40:20 +0800

    feat: setup firebase and server components router.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main >
+    <main>
       <h1>Hello next 13 🔥</h1>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
