78b7671280f90e94c9577144b93bc5b507c5c9ad
Jean Chung <jean619215@gmail.com>
2023-08-04 02:13:54 +0800

    feat: add user useContext provider.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Home = () => {
-  return (
-    <main>
-      <h1>Hello next 13 🔥</h1>
-    </main>
-  );
+  return <main>Home</main>;
 };
 
 export default Home;
